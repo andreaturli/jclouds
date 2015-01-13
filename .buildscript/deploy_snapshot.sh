f7021b7888b0226f0ffcf698ef41bef4b5a1b8eb
Andrea Turli <andrea.turli@gmail.com>
2015-01-13 14:26:48 +0100

    Create .travis.yml

---
+++ .buildscript/deploy_snapshot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Deploy a jar, source jar, and javadoc jar to Sonatype's snapshot repo.
+#
+# Adapted from https://github.com/square/retrofit/commit/a38b6bed07e162765854f1ea3a1a8f195c491031
+#
+SLUG="andreaturli/jclouds"
+JDK="oraclejdk7"
+BRANCH="1.8.x"
+
+if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
+  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping snapshot deployment: was pull request."
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+else
+  echo "Deploying snapshot..."
+  cd project
+  mvn versions:set versions:update-child-modules -DnewVersion=1.8.2-$\{revision\}-SNAPSHOT -DgenerateBackupPoms=false
+  cd ..
+  mvn clean source:jar javadoc:jar deploy --settings=".buildscript/settings.xml" -Dmaven.test.skip=true -Dsha1=`git rev-parse HEAD`
+  echo "Snapshot deployed!"
+fi
